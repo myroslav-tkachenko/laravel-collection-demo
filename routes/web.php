c6aeaae31eb3d7595d53ab102a90262bc911e2b8
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2017-04-02 18:37:39 +0300

    Basic routing examples. Create WelcomeController

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,54 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Example: Basic route
+
+// Route::get('/', function () {
+//     return 'Hello, World!';
+// });
+
+
+// Example: Sample website
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('about', function () {
+//     return view('about');
+// });
+
+// Route::get('products', function () {
+//     return view('products');
+// });
+
+// Route::get('services', function () {
+//     return view('services');
+// });
+
+
+// Example: Route verbs
+
+// Route::get('/', function () {
+//     return 'Hello, World!';
+// });
+
+// Route::post('/', function () {});
+
+// Route::put('/', function () {});
+
+// Route::delete('/', function () {});
+
+// Route::any('/', function () {});
+
+// Route::match(['get', 'post'], '/', function () {});
+
+
+// Example: Controller method
+
+Route::get('/', 'WelcomeController@index');
